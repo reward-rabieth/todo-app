fefdac94cc2b3d21b8127a830966c131f691d59a
reward rabieth <reward@DESKTOP-OUSEGKA.localdomain>
2022-08-23 09:25:43 +0300

    first commit

---
+++ src/utils/db.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { config } from "./config";
+import { logger } from "./logger";
+
+export async function connectToDb() {
+  try {
+    await mongoose.connect(config.DATABASE_URL);
+    logger.info(`connected to database`);
+  } catch (e) {
+    logger.error(e);
+    process.exit(1);
+  }
+}
+export function disconnectFromDb() {
+  return mongoose.connection.close();
+}
